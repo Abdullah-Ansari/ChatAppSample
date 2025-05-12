7d725dc2f766d1bbbf76b83e86279b4dabed59ae
Abdullah Ansari <abdullahansari018@gmail.com>
2025-05-13 00:07:57 +0530

    Initial Commit

---
+++ ChatAppSample/ChatAppSample/ChatAppSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSampleApp.swift
+//  ChatAppSample
+//
+//  Created by Abdullah Ansari on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
